1512ea9ab530da1a838789705ba4dd0030dc7d33
Follow the wind <36281137+wy414012@users.noreply.github.com>
2023-03-29 17:25:33 +0800

    Update CpuTSCSync.cpp
    测试

--- Intel/CpuTSCSync.cpp
+++ Intel/CpuTSCSync.cpp
@@ -3,76 +3,93 @@
 
 OSDefineMetaClassAndStructors(CpuTSCSync, IOService)
 
-//stamp the tsc
-extern "C" void stamp_tsc(void *tscp)
+// Stamp the TSC
+extern "C"
+void stamp_tsc(void* tscp)
 {
-	wrmsr64(MSR_IA32_TSC, *reinterpret_cast<uint64_t*>(tscp));
+    wrmsr64(MSR_IA32_TSC, *static_cast<uint64_t*>(tscp));
 }
 
-IOService* CpuTSCSync::probe(IOService* provider, SInt32* score)
-{
-    if (!super::probe(provider, score)) return NULL;
-    if (!provider) return NULL;
-
-    // only attach to last CPU
-    uint16_t threadCount = rdmsr64(MSR_CORE_THREAD_COUNT);
-    OSNumber* cpuNumber = OSDynamicCast(OSNumber, provider->getProperty("IOCPUNumber"));
-    if (!cpuNumber || cpuNumber->unsigned16BitValue() != threadCount-1)
-        return NULL;
-
-    return this;
-}
-
+// Define power states
 static IOPMPowerState powerStates[2] =
 {
     { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
     { 1, kIOPMPowerOn, kIOPMPowerOn, kIOPMPowerOn, 0, 0, 0, 0, 0, 0, 0, 0 }
 };
 
-IOReturn CpuTSCSync::setPowerState(unsigned long powerStateOrdinal, IOService *whatDevice )
+// Probe method
+IOService* CpuTSCSync::probe(IOService* provider, SInt32* score)
 {
-    if (powerStateOrdinal)
+    if (!super::probe(provider, score)) {
+        return nullptr;
+    }
+
+    if (!provider) {
+        return nullptr;
+    }
+
+    // Only attach to last CPU
+    uint16_t threadCount = rdmsr64(MSR_CORE_THREAD_COUNT);
+    OSNumber* cpuNumber = OSDynamicCast(OSNumber, provider->getProperty("IOCPUNumber"));
+    if (!cpuNumber || cpuNumber->unsigned16BitValue() != threadCount - 1) {
+        return nullptr;
+    }
+
+    return this;
+}
+
+// Set power state
+IOReturn CpuTSCSync::setPowerState(unsigned long powerStateOrdinal, IOService* whatDevice)
+{
+    if (powerStateOrdinal) {
         this->doTSC();
+    }
 
     return IOPMAckImplied;
 }
 
-void CpuTSCSync::stop(IOService *provider)
+// Stop method
+void CpuTSCSync::stop(IOService* provider)
 {
     PMstop();
     super::stop(provider);
 }
 
-bool CpuTSCSync::start(IOService *provider)
+// Start method
+bool CpuTSCSync::start(IOService* provider)
 {
-	if (!super::start(provider)) { return false; }
+    if (!super::start(provider)) {
+        return false;
+    }
 
-    // announce version
+    // Announce version
     extern kmod_info_t kmod_info;
-    IOLog("CpuTSCSync: Version %s starting on OS X Darwin %d.%d.\n", kmod_info.version, version_major, version_minor);
+    IOLog("CpuTSCSync: Version %s starting on OSX Darwin %d.%d.\n", kmod_info.version, version_major, version_minor);
 
-    // place version/build info in ioreg properties RM,Build and RM,Version
+    // Place version/build info in IO registry properties
     char buf[128];
     snprintf(buf, sizeof(buf), "%s", kmod_info.version);
-    setProperty("build,Version:", buf);
+    setProperty("build, Version:", buf);
+
 #ifdef DEBUG
     setProperty("Build-Debug:", buf);
 #else
     setProperty("Build-Release:", buf);
 #endif
 
+    // Initialize power management
     PMinit();
     registerPowerDriver(this, powerStates, 2);
     provider->joinPMtree(this);
-	return true;
+
+    return true;
 }
 
-// 更新全部处理器的MSR
+// Update TSC for all processors
 void CpuTSCSync::doTSC()
 {
-	uint64_t tsc = rdtsc64();
-    IOLog("CpuTSCSync: current tsc from rdtsc64() is %lld. Rendezvouing..\n", tsc);
-	
-	// 更新所有处理器TSC
-	mp_rendezvous_no_intrs(stamp_tsc, &tsc);
+    uint64_t tsc = rdtsc64();
+    IOLog("CpuTSCSync: current TSC from rdtsc64() is %lld. Rendezvouing..\n", tsc);
+
+    mp_rendezvous_no_intrs(stamp_tsc, &tsc);
 }
